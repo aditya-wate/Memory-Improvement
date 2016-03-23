2b42af3f6414d0ae12fc246731f89b8ac08181ce
Aditya Wate <aditya.wate@hotmail.com>
2016-03-23 00:34:33 -0400

    sql script correction, table order

--- back-end/mi_db_script.sql
+++ back-end/mi_db_script.sql
@@ -5,10 +5,11 @@ SET AUTOCOMMIT=0;
 
 SELECT 'CREATING SCHEMA STRUCTURE' as 'INFO';
 
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user, patient, guardian, quiz;
 
 /*!50503 set default_storage_engine = InnoDB */;
 /*!50503 select CONCAT('storage engine: ', @@default_storage_engine) as INFO */;
+
 
 CREATE TABLE user (
   user_id INT(11) NOT NULL AUTO_INCREMENT,
@@ -19,21 +20,21 @@ CREATE TABLE user (
   PRIMARY KEY (user_id)
 );
 
-CREATE TABLE patient (
-  patient_id INT(11) NOT NULL AUTO_INCREMENT,
-  guardian_id INT(11) NOT NULL,
-  user_id INT(11) NOT NULL,
-  /*Patient Information goes here*/
-  FOREIGN KEY (guradian_id) REFERENCES guardian (guardian_id),
-  FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
-  PRIMARY KEY (patient_id)
-);
-
 CREATE TABLE guardian (
   guardian_id INT(11) NOT NULL AUTO_INCREMENT,
   user_id INT(11) NOT NULL,
   FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
   PRIMARY KEY (guardian_id)
+);
+
+CREATE TABLE patient (
+  patient_id INT(11) NOT NULL AUTO_INCREMENT,
+  guardian_id INT(11) NOT NULL,
+  user_id INT(11) NOT NULL,
+  /*Patient Information goes here*/
+  FOREIGN KEY (guardian_id) REFERENCES guardian (guardian_id),
+  FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
+  PRIMARY KEY (patient_id)
 );
 
 
