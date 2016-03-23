829751b33ff6304f20ec8066f929bab9c95ef5f9
Aditya Wate <aditya.wate@hotmail.com>
2016-03-23 00:14:33 -0400

    sql script first draft

---
+++ back-end/mi_db_script.sql
@@ -0,0 +1,48 @@
+DROP SCHEMA IF EXISTS memory_improve;
+CREATE SCHEMA memory_improve;
+USE memory_improve;
+SET AUTOCOMMIT=0;
+
+SELECT 'CREATING SCHEMA STRUCTURE' as 'INFO';
+
+DROP TABLE IF EXISTS user;
+
+/*!50503 set default_storage_engine = InnoDB */;
+/*!50503 select CONCAT('storage engine: ', @@default_storage_engine) as INFO */;
+
+CREATE TABLE user (
+  user_id INT(11) NOT NULL AUTO_INCREMENT,
+  username CHAR(30) NOT NULL,
+  password_hash CHAR(20) NOT NULL,
+  first_name CHAR(30) NOT NULL,
+  last_name INT(30) NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE patient (
+  patient_id INT(11) NOT NULL AUTO_INCREMENT,
+  guardian_id INT(11) NOT NULL,
+  user_id INT(11) NOT NULL,
+  /*Patient Information goes here*/
+  FOREIGN KEY (guradian_id) REFERENCES guardian (guardian_id),
+  FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
+  PRIMARY KEY (patient_id)
+);
+
+CREATE TABLE guardian (
+  guardian_id INT(11) NOT NULL AUTO_INCREMENT,
+  user_id INT(11) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
+  PRIMARY KEY (guardian_id)
+);
+
+
+CREATE TABLE quiz (
+  quiz_id INT(11) NOT NULL AUTO_INCREMENT,
+  score INT NOT NULL CHECK(score <= 100),
+  patient_id INT(11) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  FOREIGN KEY (patient_id) REFERENCES patient (patient_id) ON DELETE CASCADE,
+  PRIMARY KEY (quiz_id)
+);
