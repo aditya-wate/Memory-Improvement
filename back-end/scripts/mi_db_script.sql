b30b06fee017b3f1bd371b84e15ea103ebadfd96
Aditya Wate <aditya.wate@hotmail.com>
2016-03-25 17:22:44 -0400

    db model updates

--- back-end/scripts/mi_db_script.sql
+++ back-end/scripts/mi_db_script.sql
@@ -5,7 +5,7 @@ SET AUTOCOMMIT=0;
 
 SELECT 'CREATING SCHEMA STRUCTURE' as 'INFO';
 
-DROP TABLE IF EXISTS user, patient, guardian, quiz;
+DROP TABLE IF EXISTS user, patient, guardian, quiz, question, category_city, category_food;
 
 /*!50503 set default_storage_engine = InnoDB */;
 /*!50503 select CONCAT('storage engine: ', @@default_storage_engine) as INFO */;
@@ -47,3 +47,47 @@ CREATE TABLE quiz (
   FOREIGN KEY (patient_id) REFERENCES patient (patient_id) ON DELETE CASCADE,
   PRIMARY KEY (quiz_id)
 );
+
+CREATE TABLE question (
+  question_id INT(11) NOT NULL AUTO_INCREMENT,
+  category CHAR(20) NOT NULL,
+  question_string CHAR(50) NOT NULL,
+  answer CHAR(30) NOT NULL,
+  patient_id INT(11) NOT NULL,
+  FOREIGN KEY (patient_id) REFERENCES patient (patient_id) ON DELETE CASCADE,
+  PRIMARY KEY (question_id)
+);
+
+CREATE TABLE images (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  image blob NOT NULL DEFAULT '',
+  question_id INT(11) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (question_id) REFERENCES question (question_id)
+);
+
+CREATE TABLE category_city (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  city_name char(35) NOT NULL DEFAULT '',
+  PRIMARY KEY (id)
+) ;
+
+
+CREATE TABLE category_food (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  food char(35) NOT NULL DEFAULT '',
+  PRIMARY KEY (id)	
+);
+
+INSERT INTO category_city VALUES (1, "Mumbai");
+INSERT INTO category_city VALUES (2, "Boston");
+INSERT INTO category_city VALUES (3, "Seattle");
+INSERT INTO category_city VALUES (4, "Rome");
+INSERT INTO category_city VALUES (5, "London");
+
+INSERT INTO category_food VALUES (1, "Sushi");
+INSERT INTO category_food VALUES (2, "Chicken Curry");
+INSERT INTO category_food VALUES (3, "Teriyaki Chiken");
+INSERT INTO category_food VALUES (4, "Steak");
+INSERT INTO category_food VALUES (5, "Sandwich");
+
