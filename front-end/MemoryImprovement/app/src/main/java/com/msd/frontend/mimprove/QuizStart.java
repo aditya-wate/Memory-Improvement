ce9bc9aaaf27715739e3eb8b5a2f33429ae98641
kiranprakash154 <kiranprakash154@gmail.com>
2016-04-04 16:07:06 -0400

    changes to class QuizStart

--- front-end/MemoryImprovement/app/src/main/java/com/msd/frontend/mimprove/QuizStart.java
+++ front-end/MemoryImprovement/app/src/main/java/com/msd/frontend/mimprove/QuizStart.java
@@ -1,19 +1,27 @@
 package com.msd.frontend.mimprove;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatImageButton;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.msd.frontend.mimprove.adapter.QuestionsAdapter;
-import com.msd.frontend.mimprove.interfaces.QuestionsInterfaces;
+import com.example.kiran.msdapp1.adapter.QuestionsAdapter;
+import com.example.kiran.msdapp1.design.SwipeDisabledViewPager;
+import com.example.kiran.msdapp1.interfaces.QuestionsInterfaces;
+import com.example.kiran.msdapp1.sdutils.SdUtils;
 
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 import java.util.ArrayList;
-
-/**
- * Created by Kiran on 3/23/2016.
- */
-public class QuizStart  extends AppCompatActivity implements QuestionsInterfaces
+import java.util.Map;
+public class QuizStart extends AppCompatActivity implements QuestionsInterfaces
 {
     TextView pageNo;
     @Override
@@ -24,32 +32,39 @@ public class QuizStart  extends AppCompatActivity implements QuestionsInterfaces
         ViewPager pager = (ViewPager)findViewById(R.id.questions_adapter);
         pageNo = (TextView)findViewById(R.id.page_number);
 
-        FetchQuiz fetcher = new FetchQuiz();
-        Quiz quiz = fetcher.createQuiz();
-        final ArrayList<Question> questions = quiz.getQuestions();
-        questionList.clear();
-        questionList.addAll(questions);
-        pageNo.setText("1 of "+questionList.size());
+//        createQuiz creator = new createQuiz();
 
-        QuestionsAdapter questionsAdapter = new QuestionsAdapter(questions,getSupportFragmentManager());
-        pager.setAdapter(questionsAdapter);
+//        quiz = null;
+        try {
+            SdUtils.copyJsonToSdCard(QuizStart.this);
+            UserQuiz quiz = createQuiz.getRandomisedQuiz(QuizStart.this);
+            final ArrayList<QuestionKP> questions = quiz.getQuestionList();
+            questionList.clear();
+            questionList.addAll(questions);
+            pageNo.setText("1 of "+questionList.size());
+            QuestionsAdapter questionsAdapter = new QuestionsAdapter(questions,getSupportFragmentManager());
+            pager.setAdapter(questionsAdapter);
+            pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+                {
 
-        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                }
 
-            }
+                @Override
+                public void onPageSelected(int position) {
+                    pageNo.setText("" + (position + 1) + " of " + questionList.size());
+                }
 
-            @Override
-            public void onPageSelected(int position)
-            {
-                pageNo.setText(""+(position+1)+" of "+questionList.size());
-            }
+                @Override
+                public void onPageScrollStateChanged(int state) {
 
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
