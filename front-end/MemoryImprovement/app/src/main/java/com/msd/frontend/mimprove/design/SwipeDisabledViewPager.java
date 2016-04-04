2c868c6921a01004bafbb2b9f868fcd57a2d4bbe
kiranprakash154 <kiranprakash154@gmail.com>
2016-04-04 16:13:38 -0400

    added dir design and file SwipeDisabledViewPager

---
+++ front-end/MemoryImprovement/app/src/main/java/com/msd/frontend/mimprove/design/SwipeDisabledViewPager.java
@@ -0,0 +1,31 @@
+package com.msd.frontend.mimprove.fragment;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by Kiran on 25/03/16.
+ */
+public class SwipeDisabledViewPager extends ViewPager
+{
+    public SwipeDisabledViewPager(Context context) {
+        super(context);
+    }
+
+    public SwipeDisabledViewPager(Context context,AttributeSet attributeSet)
+    {
+        super(context,attributeSet);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
