376f16f00dfc332b12c106b19be638b4991bd7b1
Aditya Wate <aditya.wate@hotmail.com>
2016-04-07 16:59:28 -0400

    removed useless code

--- back-end/mimprove/views/__init__.py
+++ back-end/mimprove/views/__init__.py
@@ -1,12 +1,3 @@
 from flask.ext.login import LoginManager
 
 login_manager = LoginManager()
-
-class Permissions(object):
-    """
-    This class currently just serves as a namespace for permission names.
-    """
-    # allows viewing basic information of a report
-    PERMISSION_VIEW_REPORT = "view_report"
-    # allows viewing all information of a report
-    PERMISSION_VIEW_FULL_REPORT = "view_full_report"
